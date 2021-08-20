209f64c698a72345f67af6f81919c634eb7487d9
valevuy <valeriavallejo91@gmail.com>
2021-08-20 18:19:41 -0300

    products

--- js/products.js
+++ js/products.js
@@ -7,7 +7,7 @@ function showProductsList(){
         let product = productsArray[i];
 
         htmlContentToAppend += `
-        <a href="category-info.html" class="list-group-item list-group-item-action">  
+        <a href="product-info.html" class="list-group-item list-group-item-action">  
             <div class="row">
                 <div class="col-3">
                     <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
@@ -33,11 +33,9 @@ function showProductsList(){
 document.addEventListener("DOMContentLoaded", function(e){ /*funcion anonima*/
     showSpinner();
     getJSONData(PRODUCTS_URL).then(function(resultObj){  /*lo tengo que procesar con un then porque lo que me devuleve un then es una promisse que se tiene que trabajar dentro de un then. Nunca salgo de la cadena de then . Se nombra el paquete que se recibe con una funcion anomima*/
-        if (resultObj.status === "ok") /* se consulta si el estatus está ok*/
-        {
-            productsArray = resultObj.data; /* a la lista vacia categoriesArray, la completa con el otro campo dle objeto que esta nombrado como resultObj y tiene un campo data*/
-            //Muestro productos ordenados
-            showProductsList(productsArray); /* muestra los elementos de la lista */
+        if (resultObj.status === "ok"){
+        
+            showProductsList(productsArray, resultObj.data); /* muestra los elementos de la lista */
         }
         hideSpinner();
     });
