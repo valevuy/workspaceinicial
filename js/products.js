2091b18e770334c6185eb76a51465d8a394c830f
valevuy <valeriavallejo91@gmail.com>
2021-08-20 17:28:52 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,31 +1,28 @@
 var productsArray = []; /* define una lista vacia*/
 
-function showProductsList(array){
+function showProductsList(){
 
     let htmlContentToAppend = "";
-    for(let i = 0; i < array.length; i++){
-        let product = array[i];
+    for(let i = 0; i < productsArray.length; i++){
+        let product = productsArray[i];
 
         htmlContentToAppend += `
-        <div class="list-group-item list-group-item-action">
+        <a href="category-info.html" class="list-group-item list-group-item-action">  
             <div class="row">
                 <div class="col-3">
-                    <img src="` + product.imgSrc + `" alt="" class="img-thumbnail">
-                    
+                    <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
                 </div>
-
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
                         <h4 class="mb-1">`+ product.name +`</h4>
-                        
                         <small class="text-muted">` + product.productCount + ` artículos</small>
                     </div>
-                    <p> `+ product.description +`</p>
+                    <p class="mb-1">` + product.description + `</p>
                 </div>
             </div>
-        </div>
-        `
-
+        </a>
+        ` //<a> </a> es para que en link sea todo clickeable. clase de boostrap. SE Puede utilizar la misma estructura para por eje productos
+        
         document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
     }
 }
