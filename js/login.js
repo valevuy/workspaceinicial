82488fc304ddc346b0fd71cc096f5b53b9e251bd
valevuy <valeriavallejo91@gmail.com>
2021-08-22 20:06:31 -0300

    ok

--- js/login.js
+++ js/login.js
@@ -1,6 +1,15 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+function validar(){
+    let usuario = document.getElementById("usuario").value;
+    let pass = document.getElementById("pass").value;
+    if((usuario!=="")&& (pass!=="")){
+        window.location.href= "./home.html";
+    }
+    else{
+        alert("debe completar los campos");
+    }
+}
 document.addEventListener("DOMContentLoaded", function(e){
-
-});
+    document.getElementById("btnLogin").addEventListener("click",function(event){
+        validar();
+    })
+})
--- js/products.js
+++ js/products.js
@@ -15,7 +15,7 @@ function showProductsList(){
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
                         <h4 class="mb-1">`+ product.name +`</h4>
-                        <small class="text-muted">` + product.soldCount + ` artículos</small>
+                        <small class="text-muted">` + product.soldCount + ` artículos vendidos</small>
                     </div>
                     <p class="mb-1">` + product.cost + product.currency + `</p>
                 </div>
